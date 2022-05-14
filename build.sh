6e366cebddfd3b8b61fa8f4e4877b7297173c735
eulerthedestroyer <eulerthedestroyer@gmail.com>
2022-05-14 08:57:00 -0700

    change

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 NODE_ENV=production 
-yarn run build
-yarn run export
+sudo yarn run build
+sudo yarn run export
 touch dist/.nojekyll
+sudo chown -R "${USER:-$(id -un)}" .
 git add dist
 git commit -m "Initial dist subtree commit"
 git subtree push --prefix dist origin gh-pages
